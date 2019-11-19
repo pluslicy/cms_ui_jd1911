f0bd3d633c0778bce28cb0273321b22686844c8e
pluslicy <licy@briup.com>
2019-11-19 19:44:06 +0800

    授权

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -85,7 +85,6 @@ const actions = {
         // reset visited views and cached views
         // to fixed https://github.com/PanJiaChen/vue-element-admin/issues/2485
         dispatch('tagsView/delAllViews', null, { root: true })
-
         resolve()
       }).catch(error => {
         reject(error)
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import { Message } from 'element-ui'
 import store from '@/store'
+import router from '@/router'
 import { getToken } from '@/utils/auth'
 
 // create an axios instance
@@ -14,7 +15,6 @@ const service = axios.create({
 service.interceptors.request.use(
   config => {
     // do something before request is sent
-
     if (store.getters.token) {
       // let each request carry token
       // ['X-Token'] is a custom headers key
@@ -51,11 +51,10 @@ service.interceptors.response.use(
     // if the custom code is not 20000, it is judged as an error. 
     if ( res.status !== 200) {
       // 消息弹框
-      Message({
-        message: res.message,
-        type: 'error',
-        duration: 5 * 1000
-      })
+      Message({ message: res.message, type: 'error', duration: 5 * 1000 })
+      if(res.status === 401){
+        logout();
+      }
       // 返回承诺失败对象
       return Promise.reject(new Error(res.message || 'Error'))
     } else {
@@ -72,5 +71,10 @@ service.interceptors.response.use(
     return Promise.reject(error)
   }
 )
+// 退出
+async function logout() {
+  await store.dispatch('user/logout')
+  router.push(`/login`)
+}
 
 export default service
